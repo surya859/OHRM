871a11fab07abae920868c1382ec0677df8429e7
HP <HP@DESKTOP-E5U7PBU>
2022-05-16 12:54:29 +0530

    Second commit

---
+++ Mydemoproject/src/demo/Variable_Demo.java
@@ -0,0 +1,27 @@
+package demo;
+
+public class Variable_Demo {
+
+	public static void main(String[] args) 
+	{
+	 
+		String [] carlist = {"car1","car2","car3"};
+		for (String carname:carlist)
+		{
+			System.out.println(carname);
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	 	
+		
+	}
+
+}
